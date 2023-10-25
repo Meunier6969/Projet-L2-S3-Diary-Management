bae84bf46c21aed1ec412c59811ef98ffbefbcb3
Meunier6969 <antoine.meunier@efrei.net>
2023-10-25 19:02:33 +0200

    Test of cell.c

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+#include "list.h"
+
 int main()
 {
-    printf("bellabito");
-    return 1;
+    t_d_cell* cell = createCell(69, 5);
+    
+    cell->next[0] = (void*) 0x69696969;
+    displayCell(*cell);
+    
+    return 0;
 }
