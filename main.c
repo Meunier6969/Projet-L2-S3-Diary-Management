5a033cc435a0488700aa6f31c88c2ec979d78de5
Meunier6969 <antoine.meunier@efrei.net>
2023-11-10 16:15:09 +0100

    timing 1/2

--- main.c
+++ main.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
 
 #include "list.h"
+#include "timer/timer.h"
+
+#define MAX_ITER 1000000
 
 int main()
 {
-    t_d_list list = createExampleList(3);
-
-    displayAlign(list);
+    int initialLevel = 1;
+    int finalLevel = 7;
     
-    // Both functions assume the list is sorted
-    printf("0: %p\n", searchClassic(list, 0));
-    printf("1: %p\n", searchClassic(list, 1));
-    printf("4: %p\n", searchClassic(list, 4));
-    printf("7: %p\n", searchClassic(list, 7));
-    printf("9: %p\n", searchClassic(list, 9));
+    t_d_list list;
 
-    printf("0: %p\n", searchHigh(list, 0));
-    printf("1: %p\n", searchHigh(list, 1));
-    printf("4: %p\n", searchHigh(list, 4));
-    printf("7: %p\n", searchHigh(list, 7));
-    printf("9: %p\n", searchHigh(list, 9));
+    FILE *log_file = fopen("log.txt", "w");
+    char *time_classic;
+    char *time_high;
+    int level;
 
+
+    for (level = initialLevel; level < finalLevel; level++)
+    {
+        list = createExampleList(level);
+
+        // Classic search
+        startTimer();
+        for (size_t i = 0; i < MAX_ITER; i++)
+        {
+            (0);
+        }
+        stopTimer();
+        time_classic = getTimeAsString();
+        
+        // High search
+        startTimer();
+        for (size_t i = 0; i < MAX_ITER; i++)
+        {
+            (0);
+        }
+        stopTimer();
+        time_high = getTimeAsString();
+        
+        // Save to file
+        fprintf(log_file, "%d\t%s\t%s\n", level, time_classic, time_high);
+    }
+
+
+
+    fclose(log_file);
     return 0;
 }
