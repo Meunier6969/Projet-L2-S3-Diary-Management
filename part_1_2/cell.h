13af3e059c39106cb2030ba51656f7221d8ce67e
Meunier6969 <antoine.meunier@efrei.net>
2023-11-12 20:22:16 +0100

    bing chilling

--- part_1_2/cell.h
+++ part_1_2/cell.h
@@ -13,27 +13,4 @@ typedef struct s_d_cell
 t_d_cell* createCell(int value, int height);
 void displayCell(t_d_cell cell);
 
-// Part 3
-typedef struct s_d_contact
-{
-    char surname[30];
-    char firstName[30];
-} t_d_contact; 
-
-typedef struct s_d_appointment
-{
-    int day;
-    int month;
-    int year;           //In it's entirety, not just 23, but 2023 (In case we go back in time u know)
-
-    int app_hour;       //Appointment time
-    int app_minute;
-
-    int lenght_hour;    //Length of appointment
-    int lenght_minute;
-
-    char purpose[];     //Information on the appointment. Can be long, so depends I guess.
-} t_d_appointment; 
-
-char *scanString(void); //"Performs the input and returns a dynamic array" whatever that means.
 #endif
