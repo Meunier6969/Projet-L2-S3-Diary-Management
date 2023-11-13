18d1de67538437eb7f90aade7cfbf71c222deb45
An0 <ainine.nassim@gmail.com>
2023-11-13 19:28:42 +0100

    This is groovy clean
    J'suis genre 80% fier.

--- src/menu.c
+++ src/menu.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "../includes/menu.h"
+
+
 
 void displayMenu()
 {
+    printf("\033[H\033[J");
     printf("\n+-------------Diary Managment--------------+");
     printf("\n+------------------------------------------+");
     printf("\n+-  Welcome, what would you like to do ?  -+");
@@ -35,37 +37,47 @@ void menuCreateContact()
     char surname[30];
     while (run==1)
     {
-        printf("\n+----------------------------+");
-        printf("\nYou choose to Create a Contact");
+        printf("\033[H\033[J");
+        printf("\n+---------------------------------------+");
+        printf("\nYou choose to create a contact.");
         printf("\nPlease enter the name of the new contact.");
-        printf("\n[Enter 'quit' to quit to the main menu]");
+        printf("\n+---------------------------------------+");
+        printf("\n[Enter 'quit' to quit to the main menu.]");
         printf("\n-> ");
         scanf("%s",name); //FAUDRA REMPLACER PAR SCAN
         if (strcmp(name,"quit")!=0)
         {
-            printf("\n+--------------------------+");
-            printf("\nSuccess");
+            printf("\033[H\033[J");
+            printf("\n+------------------------------------------------------------+");
             printf("\nYou entered [%s], please enter the surname of the contact now.",name);
-            printf("\n[Enter 'quit' to quit to the previous menu]");
+            printf("\n+------------------------------------------------------------+");
+            printf("\n[Enter 'quit' to quit to the previous menu.]");
             printf("\n-> ");
             scanf("%s",surname); //FAUDRA REMPLACER PAR SCAN
             if (strcmp(surname,"quit")!=0)
             {
-                printf("\n+-----------------------+");
+                printf("\033[H\033[J");
+                printf("\n+-----------------------------------------------------------------+");
                 printf("\nThe operation was a sucess, contact [%s] [%s] has been created.",name,surname);
-                printf("\n[Ready to continue ? You cannot go back anyway.] -> ");
+                printf("\n+-----------------------------------------------------------------+");
+                printf("\n[Ready to continue ? You cannot go back anyway.]\n-> ");
                 scanf("%s", &verif);
+                //FAUDRA METTRE LA RECUPERATION DE NOM ET SURNOM ICI OU DIRECT LA FONCTION DE CREATION ?
             }
             else if (strcmp(surname,"quit")==0)
             {
-                printf("\n+-----------------------------+");
-                printf("\nYou choose to quit, you will now go back to the precedent screen");
+                printf("\033[H\033[J");
+                printf("\n+---------------------------------------------------------------+");
+                printf("\nYou choose to quit, you will now go back to the precedent screen.");
+                printf("\n+---------------------------------------------------------------+");
             }
         }
         else if (strcmp(name,"quit")==0)
         {
-            printf("\n+------------------+");
-            printf("\nYou choose to quit.");
+            printf("\033[H\033[J");
+            printf("\n+------------------------+");
+            printf("\nYou choose to quit. Bybye.");
+            printf("\n+------------------------+");
             run=0;
         }
     }
