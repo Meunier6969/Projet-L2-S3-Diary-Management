0c5341d6c192748e67fa4144f9a0a2e9f910c9a7
Meunier6969 <antoine.meunier@efrei.net>
2023-11-25 12:06:32 +0100

    contact the aliens

--- includes/contact.h
+++ includes/contact.h
@@ -8,4 +8,8 @@ typedef struct s_d_contact
     char *firstName;
 } t_d_contact;
 
+t_d_contact* createContact(char *, char *);
+void deleteContact(t_d_contact*);
+void displayContact(t_d_contact);
+
 #endif
--- src/contact.c
+++ src/contact.c
@@ -1,1 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "../includes/contact.h"
+#include "../includes/menu.h" // access to _MAX_LENGTH
+
+t_d_contact* createContact(char *surname, char *firstName)
+{
+    t_d_contact* newContact = (t_d_contact*) malloc(sizeof(t_d_contact));
+
+    newContact->surname = surname;
+    newContact->firstName = firstName;
+
+    return newContact;
+}
+
+void deleteContact(t_d_contact* contact)
+{
+    free(contact);
+}
+
+void displayContact(t_d_contact contact)
+{
+    printf("Contact : [%s] [%s]\n", contact.surname, contact.firstName);
+}
