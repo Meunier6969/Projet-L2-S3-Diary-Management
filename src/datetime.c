7318b5b2daf12f147dde844704517fdd34e2c941
Meunier6969 <antoine.meunier@efrei.net>
2023-12-02 17:22:22 +0100

    malloced

--- src/datetime.c
+++ src/datetime.c
@@ -1,8 +1,10 @@
+#include <stdlib.h>
+
 #include "../includes/datetime.h"
 
 t_d_date *createDate(int day, int month, int year)
 {
-    t_d_date *newDate;
+    t_d_date *newDate = (t_d_date*) malloc(sizeof(t_d_date));
 
     newDate->day = day;
     newDate->month = month;
@@ -13,7 +15,7 @@ t_d_date *createDate(int day, int month, int year)
 
 t_d_length *createLength(int hours, int minutes)
 {
-    t_d_length *newLength;
+    t_d_length *newLength = (t_d_length*) malloc(sizeof(t_d_length));
 
     newLength->hours = hours;
     newLength->minutes = minutes;
